bbe4729887f8fa2f34325f60cf23924e9bf7a00c
Willyams Yujra <yracnet@gmail.com>
2025-01-16 09:06:34 -0400

    + my-ssr-app Ok

--- packages/examples/my-ssr-app/vite.config.js
+++ packages/examples/my-ssr-app/vite.config.js
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import pages from "vite-plugin-pages";
-//import ssr from "vite-plugin-ssr-config";
-import ssr from "../../plugin/src";
+import ssr from "vite-plugin-ssr-config";
+//import ssr from "../../plugin/src";
 
 export default defineConfig({
   base: "/myapp",
